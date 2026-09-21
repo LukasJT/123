78d6789e96fa86b08b6b93cbade5adfd6c8948b3
LukasJT <123797960+LukasJT@users.noreply.github.com>
2026-09-21 11:34:32 -0400

    Index films released from 1911 through 1915

--- catalog-imports.js
+++ catalog-imports.js
@@ -1,4 +1,4 @@
-window.catalogImportManifest={"total":889,"chunkSize":5000,"chunks":[{"file":"catalog-imports-1.js","count":889}]};
+window.catalogImportManifest={"total":3432,"chunkSize":5000,"chunks":[{"file":"catalog-imports-1.js","count":3432}]};
 if(typeof document!=="undefined"){
   for(const chunk of window.catalogImportManifest.chunks){
     document.write(`<script src="${chunk.file}"><\/script>`);
