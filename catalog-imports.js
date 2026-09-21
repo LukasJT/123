c1172c82b484210cdd2c2e64a788f8f3ca6d00df
LukasJT <123797960+LukasJT@users.noreply.github.com>
2026-09-21 12:19:50 -0400

    Index films released from 1926 through 1930

--- catalog-imports.js
+++ catalog-imports.js
@@ -1,4 +1,4 @@
-window.catalogImportManifest={"total":12888,"chunkSize":5000,"chunks":[{"file":"catalog-imports-1.js","count":5000},{"file":"catalog-imports-2.js","count":5000},{"file":"catalog-imports-3.js","count":2888}]};
+window.catalogImportManifest={"total":17851,"chunkSize":5000,"chunks":[{"file":"catalog-imports-1.js","count":5000},{"file":"catalog-imports-2.js","count":5000},{"file":"catalog-imports-3.js","count":5000},{"file":"catalog-imports-4.js","count":2851}]};
 if(typeof document!=="undefined"){
   for(const chunk of window.catalogImportManifest.chunks){
     document.write(`<script src="${chunk.file}"><\/script>`);
