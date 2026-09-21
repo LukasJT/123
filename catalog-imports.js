5d49dc09f65405376047f67ee082415084806d1e
LukasJT <123797960+LukasJT@users.noreply.github.com>
2026-09-21 14:04:41 -0400

    Index films released from 1941 through 1945

--- catalog-imports.js
+++ catalog-imports.js
@@ -1,4 +1,4 @@
-window.catalogImportManifest={"total":29366,"chunkSize":5000,"chunks":[{"file":"catalog-imports-1.js","count":5000},{"file":"catalog-imports-2.js","count":5000},{"file":"catalog-imports-3.js","count":5000},{"file":"catalog-imports-4.js","count":5000},{"file":"catalog-imports-5.js","count":5000},{"file":"catalog-imports-6.js","count":4366}]};
+window.catalogImportManifest={"total":34179,"chunkSize":5000,"chunks":[{"file":"catalog-imports-1.js","count":5000},{"file":"catalog-imports-2.js","count":5000},{"file":"catalog-imports-3.js","count":5000},{"file":"catalog-imports-4.js","count":5000},{"file":"catalog-imports-5.js","count":5000},{"file":"catalog-imports-6.js","count":5000},{"file":"catalog-imports-7.js","count":4179}]};
 if(typeof document!=="undefined"){
   for(const chunk of window.catalogImportManifest.chunks){
     document.write(`<script src="${chunk.file}"><\/script>`);
