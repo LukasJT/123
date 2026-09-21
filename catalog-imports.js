46b7f389583e9ec06b1b0fabd9cfa7566a72aa10
LukasJT <123797960+LukasJT@users.noreply.github.com>
2026-09-21 11:23:55 -0400

    Index films released from 1901 through 1905

--- catalog-imports.js
+++ catalog-imports.js
@@ -1,4 +1,4 @@
-window.catalogImportManifest={"total":59,"chunkSize":5000,"chunks":[{"file":"catalog-imports-1.js","count":59}]};
+window.catalogImportManifest={"total":316,"chunkSize":5000,"chunks":[{"file":"catalog-imports-1.js","count":316}]};
 if(typeof document!=="undefined"){
   for(const chunk of window.catalogImportManifest.chunks){
     document.write(`<script src="${chunk.file}"><\/script>`);
